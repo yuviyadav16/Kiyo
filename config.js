cd50916d0e84cd79c4316f9fc4435712edae7028
Mryuviyadav <thepradhan52@gmail.com>
2026-09-24 15:26:48 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js - Sara data yahan se manage hoga
-export const boardOptions = [{id: 'CBSE', name: 'CBSE Board'}, {id: 'BSEB', name: 'Bihar Board'}, /* ...baki saare */];
-export const classOptions = [{id: '9', name: 'Class 9'}, {id: '10', name: 'Class 10'}, /* ... */];
-export const examOptions = [{id: 'NEET', name: 'NEET'}, {id: 'JEE', name: 'JEE'}, /* ...200+ list */];
-export const skillOptions = [{id: 'Coding', name: 'Coding'}, {id: 'Design', name: 'UI/UX'}, /* ... */];
